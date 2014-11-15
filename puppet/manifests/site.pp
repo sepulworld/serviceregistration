1fcca802d2e6005f6791bf008862bbfd952e4844
Zane <zane.williamson@gmail.com>
2014-11-15 13:03:54 -0800

    adding puppet module and site.pp for local apply

---
+++ puppet/manifests/site.pp
@@ -0,0 +1,59 @@
+node default {
+
+  package { 'synapse':
+    ensure   => installed,
+    provider => gem,
+  }
+
+  package { 'build-essentials':
+    ensure => installed,
+  }
+
+  package { 'ruby':
+    ensure => installed,
+  }
+
+  package { 'ruby-dev':
+    ensure => installed,
+  }
+  
+  package { 'haproxy':
+    ensure => installed,
+  }
+
+  package { 'etcd':
+    ensure  => installed,
+    before  => Class['::etcd'],
+    require => [Apt::Key["Georiot"],Apt::Source["georiot"]],
+  }
+  
+  apt::key { "Georiot":
+    key        => "0211F6D4",
+    key_source => "http://puppetmaster.georiot.com:8090/binary/keyFile",
+    before     => Class['::etcd'], 
+  }
+
+  apt::source { 'georiot':
+    location     => 'http://puppetmaster.georiot.com:8090/binary',
+    include_src  => false,
+    release      => "",
+    repos        => '/';
+    before       => Class['::etcd'], 
+  }
+ 
+  class { '::etcd':
+    #discovery               => true,
+    # generate a new token for each unique cluster from https://discovery.etcd.io/new
+    # if you are following my cluster guide, then use the token you created for you cluster here
+    #discovery_token         => "",
+    #peer_election_timeout   => 500,
+    #peer_heartbeat_interval => 100,
+    addr                    => "${::ipaddress_eth0_0}:4001",
+    bind_addr               => "",
+    #peer_addr               => "${::ipaddress_eth0}:7001",
+    #peer_bind_addr          => "",
+    snapshot                => true,
+    verbose                 => false,
+    very_verbose            => false,
+  }
+}
