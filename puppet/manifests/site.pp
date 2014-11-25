0a347522554eb930e1bde28d5c5122d0890ef287
Zane <zane.williamson@gmail.com>
2014-11-24 23:33:03 -0800

    going back to hadiscover for testing

--- puppet/manifests/site.pp
+++ puppet/manifests/site.pp
@@ -3,19 +3,48 @@ node default {
   include apt
   include supervisord
 
-  file { '/etc/synapse.json.conf':
-    ensure => present,
-    before => Supervisord::Program['synapse'],
-    source => "puppet:///modules/synapse/synapse.json.conf.registration",
+  exec { 'go get github.com/adetante/hadiscover':
+    #   environment => ['GOROOT=/usr/lib/go', 'GOPATH=$HOME/go', 'PATH=$PATH:$GOROOT/bin:$GOPATH/bin'],
+    command         => "/usr/bin/go get github.com/adetante/hadiscover",
+    creates         => "/root/go/src/github.com/adetante",
   }
 
-  $build_packages = ['build-essential', 'python-pip', 'ruby', 'ruby-dev' ] 
+  file { '/etc/bash.bashrc':
+    ensure => present,
+    source => "puppet:///modules/golang/bashrc",
+    before => Exec['go get github.com/adetante/hadiscover'],
+  }
+
+  file { '/etc/haproxy/haproxy.cfg.tpl':
+    ensure => present,
+    source => "puppet:///modules/hadiscover/haproxy.cfg.tpl",
+    before => Exec['go get github.com/adetante/hadiscover'],
+  }
+
+  file { '/var/log/etcd/':
+    ensure   => directory,
+    before => Class['::etcd'],
+  }
+
+  #  file { '/etc/synapse.json.conf':
+  #  ensure => present,
+  #  before => Supervisord::Program['synapse'],
+  #  source => "puppet:///modules/synapse/synapse.json.conf",
+  #}
+
+  $go_packages = ['golang', 'golang-go.tools', 'gccgo-go', 'mercurial']
+  $build_packages = ['build-essential', 'python-pip', 'ruby', 'ruby-dev' ]
+
+  package { $go_packages:
+    ensure => installed,
+    before => Exec['go get github.com/adetante/hadiscover'],
+  }
 
   package { $build_packages:
     ensure => installed,
     before => Exec['go get github.com/adetante/hadiscover'],
   }
- 
+
   package { 'synapse':
     ensure   => installed,
     provider => gem,
@@ -31,11 +60,11 @@ node default {
     ensure  => running,
     require => Package['haproxy'],
   }
-  
+
   apt::key { "Georiot":
     key        => "0211F6D4",
     key_source => "http://puppetmaster.georiot.com:8090/binary/keyFile",
-    before     => Class['::etcd'], 
+    before     => Class['::etcd'],
   }
 
   apt::source { 'georiot':
@@ -43,14 +72,37 @@ node default {
     include_src  => false,
     release      => "",
     repos        => '/',
-    before       => Class['::etcd'], 
+    before       => Class['::etcd'],
   }
 
   apt::ppa { 'ppa:vbernat/haproxy-1.5': }
 
- 
-  supervisord::program { 'synaspe':
-    command    => 'synapse -c /etc/synapse.json.conf',
+
+  class { '::etcd':
+    #discovery               => true,
+    # generate a new token for each unique cluster from https://discovery.etcd.io/new
+    # if you are following my cluster guide, then use the token you created for you cluster here
+    #discovery_token        => "",
+    name                    => $hostname,
+    peer_election_timeout   => 500,
+    peer_heartbeat_interval => 100,
+    addr                    => "${::ipaddress_eth0_0}:4001",
+    bind_addr               => "",
+    peer_addr               => "${::ipaddress_eth0_0}:7001",
+    peer_bind_addr          => "${::ipaddress_eth0_0}:7001",
+    snapshot                => true,
+    verbose                 => false,
+    very_verbose            => false,
+  }
+
+  # Test once etcd patch is merged into synapse
+  #supervisord::program { 'synaspe':
+  #  command    => 'synapse -c /etc/synapse.json.conf',
+  #  priority => '100',
+  #}
+
+  supervisord::program { 'hadiscover':
+    command  => '/root/go/bin/hadiscover --config /etc/haproxy/haproxy.cfg.tpl --etcd http://localhost:4001 --ha /usr/sbin/haproxy --key service',
     priority => '100',
   }
 
